537446566ab823f11f742aed32af88a062abf15b
Abd ullah <moo713022@gmail.com>
2026-02-13 03:53:02 +0300

    fix: add Filament Tailwind preset - restores all button/color styles (bg-custom-*, text-danger-*, etc.)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import preset from './vendor/filament/filament/tailwind.config.preset';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    presets: [preset],
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
